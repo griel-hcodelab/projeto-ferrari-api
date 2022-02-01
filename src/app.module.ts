3333a1bc6911e51b41e981915f307af6f8e3b0f9
robertogriel <roberto@griel.com.br>
2022-02-01 08:17:29 -0300

    Configurado Prisma e TypeORM

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { PrismaModule } from './prisma/prisma.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: 
+  [
+    PrismaModule,
+  ],
+  controllers: 
+  [
+    AppController
+  ],
+  providers: [],
+})
+export class AppModule { }
---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,10 @@
+import { PrismaService } from './prisma.service';
+import { Module } from '@nestjs/common';
+
+@Module({
+    imports: [],
+    controllers: [],
+    providers: [
+        PrismaService,],
+})
+export class PrismaModule { }
